0a3fe80e839903eb51a502dbc6a6b8cd47ca22b7
Nimay Desai <62720666+NimayDesai@users.noreply.github.com>
2026-07-22 16:58:31 -0400

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
@@ -0,0 +1,12 @@
+# Last updated: 7/22/2026, 4:58:30 PM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maxProfit = max(maxProfit, prices[r]-prices[l])
+            else:
+                l = r
+            r += 1
+        return maxProfit
